1d36dac9c92e62551d8b92a7af3fda4cffcdaa93
Jørgen Vatle <Jorgen@WcAServices.net>
2018-04-26 09:58:33 +0300

    Wrapped "Hello world" notice in Bulma Hero.

--- imports/ui/pages/AsLanding.vue
+++ imports/ui/pages/AsLanding.vue
@@ -1,5 +1,11 @@
 <template>
-    <h1>Hello, World!</h1>
+    <section class="hero is-primary is-bold">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">Hello, World</h1>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
