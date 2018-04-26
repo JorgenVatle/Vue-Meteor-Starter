b8632042bb146db5b2dbbb28cef1b44fe5aeaa11
Jørgen Vatle <Jorgen@WcAServices.net>
2018-04-26 10:08:37 +0300

    Added landing subtitle.

--- imports/ui/pages/AsLanding.vue
+++ imports/ui/pages/AsLanding.vue
@@ -3,6 +3,7 @@
         <div class="hero-body">
             <div class="container">
                 <h1 class="title">Hello, World</h1>
+                <h2 class="subtitle">See the <a href="https://github.com/JorgenVatle/Vue-Meteor-Starter" target="_blank">GitHub Repo</a> for package links and other information.</h2>
             </div>
         </div>
     </section>
