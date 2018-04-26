86dab5a7b02641fa9aba81db19d59156623e2667
Jørgen Vatle <Jorgen@WcAServices.net>
2018-04-26 09:05:10 +0300

    Using Vue meteor tracker package.

--- imports/startup/client/vue.js
+++ imports/startup/client/vue.js
@@ -1,10 +1,17 @@
 import { Meteor } from 'meteor/meteor';
+import VueMeteorTracker from 'vue-meteor-tracker';
 import Vue from 'vue';
 
 import AsApp from '../../ui/AsApp.vue';
 import routerFactory from './routes';
 
 Meteor.startup(() => {
+
+    /**
+     * Vue-Meteor-Tracker
+     * @link https://github.com/meteor-vue/vue-meteor-tracker
+     */
+    Vue.use(VueMeteorTracker);
 
     /**
      * Global Vue Event Bus
